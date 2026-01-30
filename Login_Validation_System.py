3528545ae4e7f0582f3747280bafe0e114152f5f
eda142-cmd <eda142477@gmail.com>
2026-01-30 17:09:52 +0300

    Update Login_Validation_System.py

--- Login_Validation_System.py
+++ Login_Validation_System.py
@@ -1,16 +1,22 @@
-def main():
-    try:
-        number = int(input("Start countdown from: "))
-        countdown(number)
-    except ValueError:
-        print("Please enter a valid integer.")
+import re
 
-def countdown(n):
- 
-    while n > 0:
-        print(n)
-        n -= 1
-    print("Fire! ")
+def main():
+    password = input("Set a new password: ")
+    if is_strong(password):
+        print("Success: Your password is strong! ")
+    else:
+        print("Warning: Password too weak. ")
+
+def is_strong(s):
+    # ^: Start, $: End
+    # (?=.*[A-Z]): At least one uppercase
+    # (?=.*\d): At least one digit
+    # (?=.*[@$!%*?&]): At least one special character
+    # .{8,}: At least 8 characters long
+    pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$"
+    if re.search(pattern, s):
+        return True
+    return False
 
 if __name__ == "__main__":
     main()
